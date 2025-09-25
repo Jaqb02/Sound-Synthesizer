2b57ce2cea837b39f31eff371872ddc8bbb804ab
Jakub Kurzawski <jakub.kurzawski.02@gmail.com>
2025-09-25 19:04:24 +0200

    Summary

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,31 +29,46 @@
         private void InitializeComponent()
         {
             btnLoad = new Button();
+            cmbDevices = new ComboBox();
             SuspendLayout();
             // 
             // btnLoad
             // 
+            btnLoad.BackColor = SystemColors.GrayText;
             btnLoad.Location = new Point(12, 12);
             btnLoad.Name = "btnLoad";
-            btnLoad.Size = new Size(360, 337);
+            btnLoad.Size = new Size(109, 126);
             btnLoad.TabIndex = 0;
             btnLoad.Text = "Wgraj Plik";
-            btnLoad.UseVisualStyleBackColor = true;
-            btnLoad.Click += btnLoad_Click_1;
+            btnLoad.UseVisualStyleBackColor = false;
+            btnLoad.Click += btnLoad_Click;
+            // 
+            // cmbDevices
+            // 
+            cmbDevices.BackColor = SystemColors.ActiveCaption;
+            cmbDevices.FormattingEnabled = true;
+            cmbDevices.Location = new Point(127, 12);
+            cmbDevices.Name = "cmbDevices";
+            cmbDevices.Size = new Size(365, 23);
+            cmbDevices.TabIndex = 1;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(384, 361);
+            BackColor = SystemColors.MenuHighlight;
+            ClientSize = new Size(498, 426);
+            Controls.Add(cmbDevices);
             Controls.Add(btnLoad);
             Name = "Form1";
             Text = "Form1";
+            Load += Form1_Load;
             ResumeLayout(false);
         }
 
         #endregion
 
         private Button btnLoad;
+        private ComboBox cmbDevices;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,6 @@
 using NAudio.Wave;
+using System;
+using System.Windows.Forms;
 
 namespace Sound_Synthesizer
 {
@@ -7,9 +9,28 @@ namespace Sound_Synthesizer
         private WaveOutEvent outputDevice;
         private AudioFileReader audioFile;
 
+        // Gotowe pliki audio w folderze projektu
+        private string soundQ = @"Sounds\soundQ.wav.mp3";
+        private string soundW = @"Sounds\soundW.wav.mp3";
+        private string soundE = @"Sounds\soundE.mp3";
+
         public Form1()
         {
             InitializeComponent();
+            KeyPreview = true;
+            this.KeyDown += Form1_KeyDown;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < WaveOut.DeviceCount; i++)
+            {
+                var deviceInfo = WaveOut.GetCapabilities(i);
+                cmbDevices.Items.Add($"{i}: {deviceInfo.ProductName}");
+            }
+
+            if (cmbDevices.Items.Count > 0)
+                cmbDevices.SelectedIndex = 0;
         }
 
         private void btnLoad_Click(object sender, EventArgs e)
@@ -26,27 +47,38 @@ namespace Sound_Synthesizer
 
         private void PlaySound(string filePath)
         {
+            if (!System.IO.File.Exists(filePath))
+            {
+                MessageBox.Show($"Nie znaleziono pliku: {filePath}");
+                return;
+            }
+
             outputDevice?.Stop();
             outputDevice?.Dispose();
             audioFile?.Dispose();
 
-            outputDevice = new WaveOutEvent();
+            int deviceNumber = cmbDevices.SelectedIndex;
+            outputDevice = new WaveOutEvent() { DeviceNumber = deviceNumber };
             audioFile = new AudioFileReader(filePath);
             outputDevice.Init(audioFile);
             outputDevice.Play();
         }
 
-        private void btnLoad_Click_1(object sender, EventArgs e)
+        // Hotkeye Q/W/E odtwarzaj¹ dŸwiêki od razu
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
-            using (OpenFileDialog ofd = new OpenFileDialog())
+            switch (e.KeyCode)
             {
-                ofd.Filter = "Pliki audio|*.mp3;*.wav;*.mp4";
-                if (ofd.ShowDialog() == DialogResult.OK)
-                {
-                    PlaySound(ofd.FileName);
-                }
+                case Keys.NumPad0:
+                    PlaySound(soundQ);
+                    break;
+                case Keys.W:
+                    PlaySound(soundW);
+                    break;
+                case Keys.E:
+                    PlaySound(soundE);
+                    break;
             }
-
         }
     }
 }
